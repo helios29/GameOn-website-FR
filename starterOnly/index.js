5fac16f79c55443a774bc8f13581d6790ed29e15
Adrien Suignard <adrien.suignard@gmail.com>
2023-01-03 13:00:46 +0100

    radio button

--- starterOnly/index.js
+++ starterOnly/index.js
@@ -4,6 +4,8 @@ const form = document.getElementById("form");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,6 +18,8 @@ function checkInputs() {
   const firstNameValue = firstName.value.trim();
   const lastNameValue = lastName.value.trim();
   const emailValue = email.value.trim();
+  const birthDateValue = birthDate.value.trim();
+  const quantityValue = quantity.value.trim();
 
   if (firstNameValue === "") {
     setErrorFor(firstName, "le prénom ne peut pas rester vide");
@@ -36,18 +40,63 @@ function checkInputs() {
   } else {
     setSuccessFor(email);
   }
+
+  if (birthDateValue === "") {
+    setErrorFor(birthDate, "la date de naissance ne peut pas rester vide");
+    // } else if (!isBirthDate(birthDateValue)) {
+    //   setErrorFor(birthDate, "date de naissance invalide");
+  } else {
+    setSuccessFor(birthDate);
+  }
+
+  if (quantityValue === "") {
+    setErrorFor(quantity, "ce champ ne peut pas rester vide");
+  } else {
+    setSuccessFor(quantity);
+  }
 }
 
+//radio button
+const btn = document.querySelector(".btn-submit");
+const radioButtons = document.querySelectorAll(".checkbox-label");
+// const radioButtons = document.querySelectorAll('input[name="location"]');
+btn.addEventListener("click", () => {
+  let selectedSize;
+  for (const radioButton of radioButtons) {
+    if (radioButton.checked) {
+      selectedSize = radioButton.value;
+      break;
+    }
+  }
+  // show the output:
+  console.log(selectedSize);
+  if (selectedSize == undefined) {
+    console.log(`You haven't selected any city`);
+    setErrorForRadio(radioButtons, "ce champ ne peut pas rester vide");
+  } else {
+    console.log(`You selected ${selectedSize}`);
+    setSuccessFor(radioButtons);
+  }
+});
+
+function setErrorForRadio(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  console.log(`Valeur de Formcontrol Radio : ${formControl}`);
+  small.innerText = message;
+}
+
+//ERROR SUCCESS
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
   const small = formControl.querySelector("small");
-  formControl.className = "form-control error";
+  formControl.className = "formData error";
   small.innerText = message;
 }
 
 function setSuccessFor(input) {
   const formControl = input.parentElement;
-  formControl.className = "form-control success";
+  formControl.className = "formData success";
 }
 
 function isEmail(email) {
@@ -56,8 +105,13 @@ function isEmail(email) {
   );
 }
 
-//fermer la modal avec la croix
+function isBirthDate(birthDate) {
+  return /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
+    birthDate
+  );
+}
 
+//fermer la modal avec la croix
 const btnClose = document.querySelector(".close");
 
 btnClose.addEventListener("click", function () {
