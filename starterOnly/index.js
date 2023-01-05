3386b0e42f08cf53af9c9e238cb86d26d7f5f1b1
Adrien Suignard <adrien.suignard@gmail.com>
2023-01-05 12:57:35 +0100

    final version

--- starterOnly/index.js
+++ starterOnly/index.js
@@ -6,6 +6,8 @@ const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthDate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
+const closeBtnRed = document.getElementById("closeBtnRed");
+const confirmationMessage = document.getElementById("confirmationMessage");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,12 +25,14 @@ function checkInputs() {
   const radioButtons = document.querySelectorAll('input[name="location"]');
   const checkBox1 = document.getElementById("checkbox1");
 
-  console.log("checkbox : ", checkBox1.value);
+  var error = false; //pas d'erreur
+
   if (!checkBox1.checked) {
     setErrorFor(
       checkBox1,
       "Vous devez vérifier que vous acceptez les termes et conditions."
     );
+    error = true;
   } else {
     setSuccessFor(checkBox1);
   }
@@ -38,47 +42,56 @@ function checkInputs() {
     if (radioButton.checked) {
       selectedSize = radioButton.value;
       setSuccessFor(radioButton);
+      console.log(error);
+      error = false;
       break;
     }
     setErrorFor(radioButton, "Vous devez choisir une option.");
+    error = true;
   }
+  console.log(error);
 
   if (firstNameValue === "") {
     setErrorFor(firstName, "le prénom ne peut pas rester vide");
+    error = true;
   } else {
     setSuccessFor(firstName);
   }
 
   if (lastNameValue === "") {
     setErrorFor(lastName, "le nom ne peut pas rester vide");
+    error = true;
   } else {
     setSuccessFor(lastName);
   }
 
   if (emailValue === "") {
     setErrorFor(email, "l'email ne peut pas rester vide");
+    error = true;
   } else if (!isEmail(emailValue)) {
     setErrorFor(email, "email invalide");
+    error = true;
   } else {
     setSuccessFor(email);
   }
 
   if (birthDateValue === "") {
     setErrorFor(birthDate, "Vous devez entrer votre date de naissance.");
-    // } else if (!isBirthDate(birthDateValue)) {
-    //   setErrorFor(birthDate, "date de naissance invalide");
+    error = true;
   } else {
     setSuccessFor(birthDate);
   }
 
   if (quantityValue === "") {
     setErrorFor(quantity, "ce champ ne peut pas rester vide");
+    error = true;
   } else {
     setSuccessFor(quantity);
   }
+  return error;
 }
 
-//ERROR SUCCESS
+//ERROR SUCCESS INITIALIZE
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
   const small = formControl.querySelector("small");
@@ -91,8 +104,13 @@ function setSuccessFor(input) {
   formControl.className = "formData success";
 }
 
+function initialize(input) {
+  const formControl = input.parentElement;
+  formControl.className = "formData";
+}
+
 function isEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
     email
   );
 }
@@ -107,6 +125,45 @@ function isBirthDate(birthDate) {
 const btnClose = document.querySelector(".close");
 
 btnClose.addEventListener("click", function () {
-  console.log("Cross button clicked");
   document.querySelector(".bground").setAttribute("style", "display: none");
+});
+
+//Valider le formulaire
+form.addEventListener("submit", function (event) {
+  let result = checkInputs();
+
+  if (!result) {
+    //result === false
+    document
+      .querySelector("#confirmationMessage")
+      .setAttribute("style", "display: block");
+    document
+      .querySelector("#closeBtnRed")
+      .setAttribute("style", "display: block");
+    document
+      .querySelector(".modal-body")
+      .setAttribute("style", "display: none");
+  }
+});
+
+//fermer la modal de validation avec le bouton
+
+closeBtnRed.addEventListener("click", function () {
+  document.querySelector(".bground").setAttribute("style", "display: none");
+  document
+    .querySelector("#confirmationMessage")
+    .setAttribute("style", "display: none");
+  document.querySelector("#closeBtnRed").setAttribute("style", "display: none");
+  document.querySelector(".modal-body").setAttribute("style", "display: block");
+
+  firstName.value = "";
+  initialize(firstName);
+  lastName.value = "";
+  initialize(lastName);
+  email.value = "";
+  initialize(email);
+  birthDate.value = "";
+  initialize(birthDate);
+  quantity.value = "";
+  initialize(quantity);
 });
