c562516f89798f634aae57c10ec4ab9208e2142a
Adrien Suignard <adrien.suignard@gmail.com>
2023-01-10 17:31:40 +0100

    final version2

--- starterOnly/index.js
+++ starterOnly/index.js
@@ -6,8 +6,18 @@ const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthDate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
+const radioButtons = document.querySelectorAll('input[name="location"]');
+let radioButton;
 const closeBtnRed = document.getElementById("closeBtnRed");
 const confirmationMessage = document.getElementById("confirmationMessage");
+
+let firstNameError = "";
+let lastNameError = "";
+let emailError = "";
+let birthDateError = "";
+let quantityError = "";
+let radioButtonError = "";
+let checkBox1Error = "";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -22,80 +32,80 @@ function checkInputs() {
   const emailValue = email.value.trim();
   const birthDateValue = birthDate.value.trim();
   const quantityValue = quantity.value.trim();
-  const radioButtons = document.querySelectorAll('input[name="location"]');
   const checkBox1 = document.getElementById("checkbox1");
 
-  var error = false; //pas d'erreur
+  // var error = false; //pas d'erreur
+
+  if (firstNameValue === "") {
+    setErrorFor(firstName, "le prénom ne peut pas rester vide");
+    firstNameError = true;
+  } else {
+    setSuccessFor(firstName);
+    firstNameError = false;
+  }
+
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "le nom ne peut pas rester vide");
+    lastNameError = true;
+  } else {
+    setSuccessFor(lastName);
+    lastNameError = false;
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "l'email ne peut pas rester vide");
+    emailError = true;
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "email invalide");
+    emailError = true;
+  } else {
+    setSuccessFor(email);
+    emailError = false;
+  }
+
+  if (birthDateValue === "") {
+    setErrorFor(birthDate, "Vous devez entrer votre date de naissance.");
+    birthDateError = true;
+  } else {
+    setSuccessFor(birthDate);
+    birthDateError = false;
+  }
+
+  if (quantityValue === "") {
+    setErrorFor(quantity, "ce champ ne peut pas rester vide");
+    quantityError = true;
+  } else {
+    setSuccessFor(quantity);
+    quantityError = false;
+  }
+
+  for (radioButton of radioButtons) {
+    if (radioButton.checked) {
+      setSuccessFor(radioButton);
+      radioButtonError = false;
+      break;
+    }
+    setErrorFor(radioButton, "Vous devez choisir une option.");
+    radioButtonError = true;
+  }
 
   if (!checkBox1.checked) {
     setErrorFor(
       checkBox1,
       "Vous devez vérifier que vous acceptez les termes et conditions."
     );
-    error = true;
+    checkBox1Error = true;
   } else {
     setSuccessFor(checkBox1);
+    checkBox1Error = false;
   }
-
-  for (const radioButton of radioButtons) {
-    let selectedSize;
-    if (radioButton.checked) {
-      selectedSize = radioButton.value;
-      setSuccessFor(radioButton);
-      console.log(error);
-      error = false;
-      break;
-    }
-    setErrorFor(radioButton, "Vous devez choisir une option.");
-    error = true;
-  }
-  console.log(error);
-
-  if (firstNameValue === "") {
-    setErrorFor(firstName, "le prénom ne peut pas rester vide");
-    error = true;
-  } else {
-    setSuccessFor(firstName);
-  }
-
-  if (lastNameValue === "") {
-    setErrorFor(lastName, "le nom ne peut pas rester vide");
-    error = true;
-  } else {
-    setSuccessFor(lastName);
-  }
-
-  if (emailValue === "") {
-    setErrorFor(email, "l'email ne peut pas rester vide");
-    error = true;
-  } else if (!isEmail(emailValue)) {
-    setErrorFor(email, "email invalide");
-    error = true;
-  } else {
-    setSuccessFor(email);
-  }
-
-  if (birthDateValue === "") {
-    setErrorFor(birthDate, "Vous devez entrer votre date de naissance.");
-    error = true;
-  } else {
-    setSuccessFor(birthDate);
-  }
-
-  if (quantityValue === "") {
-    setErrorFor(quantity, "ce champ ne peut pas rester vide");
-    error = true;
-  } else {
-    setSuccessFor(quantity);
-  }
-  return error;
 }
 
 //ERROR SUCCESS INITIALIZE
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
   formControl.className = "formData error";
+  const small = formControl.querySelector("small");
   small.innerText = message;
 }
 
@@ -115,12 +125,6 @@ function isEmail(email) {
   );
 }
 
-function isBirthDate(birthDate) {
-  return /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
-    birthDate
-  );
-}
-
 //fermer la modal avec la croix
 const btnClose = document.querySelector(".close");
 
@@ -130,9 +134,15 @@ btnClose.addEventListener("click", function () {
 
 //Valider le formulaire
 form.addEventListener("submit", function (event) {
-  let result = checkInputs();
-
-  if (!result) {
+  if (
+    !firstNameError &&
+    !lastNameError &&
+    !emailError &&
+    !birthDateError &&
+    !quantityError &&
+    !radioButtonError &&
+    !checkBox1Error
+  ) {
     //result === false
     document
       .querySelector("#confirmationMessage")
@@ -147,9 +157,7 @@ form.addEventListener("submit", function (event) {
 });
 
 //fermer la modal de validation avec le bouton
-
 closeBtnRed.addEventListener("click", function () {
-  document.querySelector(".bground").setAttribute("style", "display: none");
   document
     .querySelector("#confirmationMessage")
     .setAttribute("style", "display: none");
@@ -166,4 +174,5 @@ closeBtnRed.addEventListener("click", function () {
   initialize(birthDate);
   quantity.value = "";
   initialize(quantity);
+  radioButton.checked = false;
 });
